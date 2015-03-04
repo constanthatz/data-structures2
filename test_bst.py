9a6c26843306a2c9e1ed000c391023aae218bc69
henrykh <henryhowes@gmail.com>
2015-03-04 14:13:26 -0800

    adds tests for insert, contains, size

---
+++ test_bst.py
@@ -0,0 +1,44 @@
+import pytest
+from bst import Tree
+
+
+@pytest.fixture(scope="function")
+def test_tree():
+    test_tree = Tree()
+    test_tree.insert(5)
+    test_tree.insert(6)
+    test_tree.insert(8)
+
+    return test_tree
+
+
+def test_insert_as_root():
+    test_tree = Tree()
+    test_tree.insert(5)
+    assert test_tree.root.key == 5
+
+
+def test_insert_greater():
+    test_tree = Tree()
+    test_tree.insert(5)
+    test_tree.insert(6)
+    assert test_tree.root.right.key == 6
+
+
+def test_insert_less():
+    test_tree = Tree()
+    test_tree.insert(5)
+    test_tree.insert(4)
+    assert test_tree.root.left.key == 4
+
+
+def test_contains(test_tree):
+    assert test_tree.contains(8) is True
+
+
+def test_does_not_contains(test_tree):
+    assert test_tree.contains(9) is False
+
+
+def test_size(test_tree):
+    assert test_tree.size() == 3
