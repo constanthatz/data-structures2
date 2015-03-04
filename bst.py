5b3ff805f5648fd5b9c2a55bfab07f3e04f1d8e3
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-04 11:45:44 -0800

    intialize tree class.

--- bst.py
+++ bst.py
@@ -5,3 +5,9 @@ class Leaf(object):
         self.key = key
         self.value = value
         self.right = right
+
+
+class Tree(object):
+    '''Create tree'''
+    def __init__(self):
+        self.root = None
