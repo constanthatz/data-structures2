3b79fb2a6eaa66d22d71582bbe97d8d8224e0bcb
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-17 14:42:28 -0700

    fix merge

--- merge_sort.py
+++ merge_sort.py
@@ -33,59 +33,33 @@ def merge(list1, list2):
                 new_list.append(list2.pop(0))
         except IndexError:
             new_list.append(list1.pop(0))
-    new_list.append(list2[:])
+    new_list.extend(list2)
 
-
-
-
-
-
-
-
-
-    list_len = len(lists[0])
-
-    while list_len / 2 > 1:
-
-        list_len = list_len / 2
-        for a_list in lists:
-
-
-
-
-
-        lists = [[the_list[0:list_len]], [[the_list]]
-
-
-
-
-    for i in xrange(0, len(the_list), 2):
-        if the_list[i] < len(the_list):
-            if the_list[i+1] > the_list[i]:
-                the_list[i], the_list[i+1] = the_list[i+1], the_list[i] 
-            else:
-                pass
+    return new_list
 
 if __name__ == '__main__':
         nums_best = [range(0, 10**i) for i in xrange(6)]
-        nums_worst = [item[::-1] for item in nums_best]
+        # nums_worst = [item[::-1] for item in nums_best]
 
-        time_best = []
-        time_worst = []
+        # time_best = []
+        # time_worst = []
 
-        for i in xrange(len(nums_best)):
-            def test(nums):
-                merge_sort(nums)
+        # for i in xrange(len(nums_best)):
+        #     def test(nums):
+        #         merge_sort(nums)
 
-            setup = "from __main__ import test\nnums = {}"
+        #     setup = "from __main__ import test\nnums = {}"
 
-            time_best.append(timeit.Timer(
-                "test(nums)",
-                setup=setup.format(nums_best[i])).timeit(number=10000))
-            time_worst.append(timeit.Timer(
-                "test(nums)",
-                setup=setup.format(nums_worst[i])).timeit(number=10000))
+        #     time_best.append(timeit.Timer(
+        #         "test(nums)",
+        #         setup=setup.format(nums_best[i])).timeit(number=10000))
+        #     time_worst.append(timeit.Timer(
+        #         "test(nums)",
+        #         setup=setup.format(nums_worst[i])).timeit(number=10000))
 
-        print(time_best)
-        print(time_worst)
+        # print(time_best)
+        # print(time_worst)
 
+        list1 = [3, 7]
+        list2 = [1, 13]
+        print(merge(list1, list2))
