e4378dccb6e121e590b59028271d435680ed30e7
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-18 13:28:55 -0700

    it works

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,5 @@
+import random
+
 
 def quicksort(A, lo, hi):
     if lo < hi:
@@ -12,7 +14,7 @@ def partition(A, lo, hi):
 
     A[pivotIndex], A[hi] = A[hi], A[pivotIndex]
     storeIndex = lo
-    for i in xrange(lo, hi-1):
+    for i in xrange(lo, hi):
         if A[i] < pivotValue:
             A[storeIndex], A[i] = A[i], A[storeIndex]
             storeIndex += 1
@@ -20,11 +22,16 @@ def partition(A, lo, hi):
     return storeIndex
 
 
-def select_pivot(a_list):
-
-    if a_list[0] <= a_list[len(a_list)/2] <= a_list[-1] or a_list[-1] <= a_list[len(a_list)/2] <= a_list[0]:
-        return a_list[len(a_list)/2]
-    elif a_list[len(a_list)/2] <= a_list[0] <= a_list[-1] or a_list[-1] <= a_list[0] <= a_list[len(a_list)/2]:
-        return a_list[0]
+def select_pivot(a_list, lo, hi):
+    if a_list[lo] <= a_list[(hi - lo)/2] <= a_list[hi] or a_list[hi] <= a_list[(hi - lo)/2] <= a_list[lo]:
+        return a_list[(hi - lo)/2]
+    elif a_list[(hi - lo)/2] <= a_list[lo] <= a_list[hi] or a_list[hi] <= a_list[lo] <= a_list[(hi - lo)/2]:
+        return a_list[lo]
     else:
-        return a_list[-1]
+        return a_list[hi]
+
+if __name__ == '__main__':
+    a_list = random.sample(range(10), 10)
+    print(a_list)
+    quicksort(a_list, 0, len(a_list)-1)
+    print(a_list)
