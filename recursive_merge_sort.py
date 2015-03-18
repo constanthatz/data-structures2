dfc1b5f2696faa09fc75b16780dd812db7116038
henrykh <henryhowes@gmail.com>
2015-03-17 21:03:22 -0700

    adds improved recursive merge sort

--- recursive_merge_sort.py
+++ recursive_merge_sort.py
@@ -2,35 +2,52 @@ import timeit
 
 
 def merge_sort(a_list):
-    if len(a_list) <= 1:
-        return a_list
+    if len(a_list) > 1:
+        middle = len(a_list) / 2
+        left = a_list[:middle]
+        right = a_list[middle:]
 
-    middle = len(a_list) / 2
-    left = a_list[:middle]
-    right = a_list[middle:]
+        merge_sort(left)
+        merge_sort(right)
 
-    left = merge_sort(left)
-    right = merge_sort(right)
+        left_idx, right_idx, a_list_idx = 0, 0, 0
+        while left_idx < len(left) and right_idx < len(right):
+            if left[left_idx] < right[right_idx]:
+                a_list[a_list_idx] = left[left_idx]
+                left_idx += 1
+            else:
+                a_list[a_list_idx] = right[right_idx]
+                right_idx += 1
+            a_list_idx += 1
 
-    return merge(left, right)
+        while left_idx < len(left):
+            a_list[a_list_idx] = left[left_idx]
+            left_idx += 1
+            a_list_idx += 1
 
+        while right_idx < len(right):
+            a_list[a_list_idx] = right[right_idx]
+            right_idx += 1
+            a_list_idx += 1
 
-def merge(left, right):
-    result = []
-    while left and right:
-        if left[0] <= right[0]:
-            result.append(left[0])
-            left = left[1:]
-        else:
-            result.append(right[0])
-            right = right[1:]
-    while left:
-        result.append(left[0])
-        left = left[1:]
-    while right:
-        result.append(right[0])
-        right = right[1:]
-    return result
+    return a_list
+
+# def merge(left, right):
+#     result = []
+#     while left and right:
+#         if left[0] <= right[0]:
+#             result.append(left[0])
+#             left = left[1:]
+#         else:
+#             result.append(right[0])
+#             right = right[1:]
+#     while left:
+#         result.append(left[0])
+#         left = left[1:]
+#     while right:
+#         result.append(right[0])
+#         right = right[1:]
+#     return result
 
 
 if __name__ == '__main__':
